24462745de9ba3d4c93e9440a6a9563eb20fc58f
Avokados <avokados99@gmail.com>
2022-11-11 16:12:05 +0700

    fix reduct without css

--- src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
+++ src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
@@ -11,24 +11,23 @@ const FriendMessage = (props: any) => {
             <div className={s.friendImageAndText}>
                 <img
                     id={'hw1-friend-avatar-' + props.message.id}
-                    // создаёт студент
-
-                    //
+                    src = {props.message.user.avatar}
                 />
                 <div className={s.friendText}>
                     <div
                         id={'hw1-friend-name-' + props.message.id}
                         className={s.friendName}
                     >
-                        {/*создаёт студент*/}
+                        <h1>{props.message.user.name}</h1>
 
-                        {/**/}
+
                     </div>
                     <pre
                         id={'hw1-friend-text-' + props.message.id}
                         className={s.friendMessageText}
                     >
                         {/*создаёт студент*/}
+                        <p>{props.message.message.text}</p>
 
                         {/**/}
                     </pre>
@@ -39,7 +38,7 @@ const FriendMessage = (props: any) => {
                 className={s.friendTime}
             >
                 {/*создаёт студент*/}
-
+                <p>{props.message.message.time}</p>
                 {/**/}
             </div>
         </div>
